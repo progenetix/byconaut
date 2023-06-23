8a5c8d1219b6417daa2849057f0e697be521255d
Michael Baudis <mbaudis@me.com>
2023-06-23 17:11:29 +0200

    housekeeping script with counts

--- bin/housekeeping.py
+++ bin/housekeeping.py
@@ -2,6 +2,7 @@
 
 import re, json, yaml, sys, datetime
 from copy import deepcopy
+from isodate import date_isoformat
 from os import path, environ, pardir
 from pymongo import MongoClient, GEOSPHERE
 from progress.bar import Bar
@@ -36,11 +37,29 @@ def housekeeping():
 
     todos = {
         "individual_age_days": input("Recalculate `age_days` in individuals?\n(y|N): "),
+        "datasets_counts": input("Recalculate counts for all datasets?\n(y|N): "),
         "mongodb_index_creation": input("Check & create MongoDB indexes?\n(y|N): ")
     }
 
     ds_id = byc["dataset_ids"][0]
     data_db = MongoClient( )[ ds_id ]
+
+
+    #>---------------------- info db update ----------------------------------<#
+
+    if "y" in todos.get("datasets_counts", "n").lower():
+
+        i_db = byc[ "config" ][ "services_db" ]
+        i_coll = byc[ "config" ][ "beacon_info_coll"]
+
+        print(f'==> Updating dataset statistics in "{ds_id}.{i_coll}"')
+
+        b_info = __dataset_update_counts(byc)
+
+        info_coll = MongoClient( )[ i_db ][ i_coll ]
+        info_coll.delete_many( { "date": b_info["date"] } ) #, upsert=True
+        info_coll.insert_one( b_info ) #, upsert=True 
+        print(f'==> updated entry {b_info["date"]} in {ds_id}.{i_coll}')
 
     #>------------------------ individuals -----------------------------------<#
 
@@ -71,7 +90,7 @@ def housekeeping():
     #>-------------------- MongoDB index updates -----------------------------<#
 
     if "y" in todos.get("mongodb_index_creation", "n").lower():
-        __update_mongodb_indexes(byc)
+        __update_mongodb_indexes(ds_id, byc)
 
     #>------------------- / MongoDB index updates ----------------------------<#
 
@@ -79,11 +98,12 @@ def housekeeping():
 #################################### subs ######################################
 ################################################################################
 
-def __update_mongodb_indexes(byc):
+def __update_mongodb_indexes(ds_id, byc):
 
     dt_m = byc["datatable_mappings"]
     b_rt_s = byc["service_config"]["response_types"]
-
+    mongo_client = MongoClient( )
+    data_db = mongo_client[ds_id]
     for r_t, r_d in b_rt_s.items():
 
         collname = r_d.get("collection", False)
@@ -109,6 +129,42 @@ def __update_mongodb_indexes(byc):
             print(m)
 
 ################################################################################
+
+def __dataset_update_counts(byc):
+
+    b_info = { "date": date_isoformat(datetime.datetime.now()), "datasets": { } }
+    mongo_client = MongoClient( )
+
+    # this is independend of the dataset selected for the script & will update
+    # for all in any run
+    dbs = mongo_client.list_database_names()
+    for i_ds_id in byc["dataset_definitions"].keys():
+        if not i_ds_id in dbs:
+            print(f'¡¡¡ Dataset "{i_ds_id}" does not exist !!!')
+            continue
+
+        ds_db = mongo_client[ i_ds_id ]
+        b_i_ds = { "counts": { }, "updated": datetime.datetime.now().isoformat() }
+        c_n = ds_db.list_collection_names()
+        for c in byc["config"]["queried_collections"]:
+            if c not in c_n:
+                continue
+
+            no = ds_db[ c ].estimated_document_count()
+            b_i_ds["counts"].update( { c: no } )
+            if c == "variants":
+                v_d = { }
+                bar = Bar(i_ds_id+' variants', max = no, suffix='%(percent)d%%'+" of "+str(no) )
+                for v in ds_db[ c ].find({ "variant_internal_id": {"$exists": True }}):
+                    v_d[ v["variant_internal_id"] ] = 1
+                    bar.next()
+                bar.finish()
+                b_i_ds["counts"].update( { "variants_distinct": len(v_d.keys()) } )
+
+        b_info["datasets"].update({i_ds_id: b_i_ds})
+    
+    return b_info
+
 ################################################################################
 ################################################################################
 
